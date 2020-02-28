159403693a93e27dd8a7aa8c1900b1369c5dafeb
Student <example@example.com>
2020-02-28 12:05:33 -0800

    Divide section for bread and pastry

--- bakery.cs
+++ bakery.cs
@@ -7,6 +7,18 @@ public class Bakery {
 
     static void Main()
     {
+        Bread bagel = new Bread();
+        bagel.Name ="Bagel";
+        bagel.Price = 5;
+        bagel.Loaves = 10;
+        Breads.Add(bagel);
+
+        Pastry maccaron = new Pastry();
+        maccaron.Name = "Maccaron";
+        maccaron.Price = 2;
+        maccaron.Loaves = 10;
+        Pastries.Add(maccaron);
+
         Console.WriteLine("Welcome to Pierre's Bakery");
         Console.WriteLine("Would you like to get some fresh bakeries? Please answer with this format, Y for Yes & N for N");
 
@@ -17,6 +29,8 @@ public class Bakery {
             
             BreadBrowsing();
             PastryBrowsing();
+            BreadShopping();
+            PastryShopping();
         }
         else
         {
@@ -30,61 +44,30 @@ public class Bakery {
 
     static void BreadBrowsing()
     {
-        Bread bagel = new Bread();
-        bagel.Name ="Bagel";
-        bagel.Price = 5;
-        bagel.Loaves = 10;
-
-        Breads.Add(bagel);
-
-        
-
         // foreach(Bread bread in Breads)
         //     {
         Console.Write("We have " + Breads[0].Name + " in the bread section." + " The price is $" + Breads[0].Price + ".");
-        Console.Write(" Would you buy some " + Breads[0].Name + "?" + " Please enter a number that you want to get.");
-        int userBreadNumber = int.Parse(Console.ReadLine());
-        Console.WriteLine("The cost is " + Breads[0].Buying(userBreadNumber) + ".");
             // }
     }
 
     static void PastryBrowsing()
     {
-        Pastry maccaron = new Pastry();
-        maccaron.Name = "Maccaron";
-        maccaron.Price = 2;
-        maccaron.Loaves = 10;
-        Pastries.Add(maccaron);
-
-
-
-        Console.Write("We have " + Pastries[0].Name + " in the pastry section. " + "The price is $" + Pastries[0].Price + ".");
-        Console.Write(" Would you buy some " + Pastries[0].Name + "?" + " Please enter a number that you want to get");
-        int userPastryNumber = int.Parse(Console.ReadLine());
-        Console.WriteLine("The cost is " + Pastries[0].Buying(userPastryNumber) + ".");
-
+        Console.WriteLine(" For the pastry section, We also have " + Pastries[0].Name + " with the price for each $" + Pastries[0].Price + ".");
     }
 
+    static void BreadShopping()
+    {
+        Console.Write("Would you like to buy some breads? Enter a number that you want to get.");
+        int userBreadNumber = int.Parse(Console.ReadLine());
+        Console.WriteLine("The cost is " + Breads[0].Buying(userBreadNumber) + ".");
+    }
 
-    // static void BakeryShopping()
-    // {
-       
-        
-    //     List<Pastry> Pastries = new List<Pastry> () {pastry};
-           
-
-    //     Console.WriteLine("Please enter a bakery type and numbers");
-    //     int userBakery = int.Parse(Console.ReadLine());
-
-            
-
-    //         foreach(Pastry pastries in Pastries)
-    //         {
-    //             Console.Write(pastry.Name);
-    //             Console.Write(pastry.Price);
-    //             // Console.WriteLine(pastry.Loaves);
-    //         }
-    // }
+    static void PastryShopping()
+    {
+        Console.Write("Would you like to buy some pastries? Enter a number that you want to get.");
+        int userPastryNumber = int.Parse(Console.ReadLine());
+        Console.WriteLine("The cost is " + Pastries[0].Buying(userPastryNumber) + ".");
+    }
 }
 public class Bread 
 {
@@ -104,6 +87,11 @@ public class Bread
        return Price * loaves;
        }
    }
+
+   public bool outOfStock (int loaves)
+   {
+       return (loaves > Loaves);
+   }
 }
 
 public class Pastry {
@@ -116,12 +104,17 @@ public class Pastry {
    {
        if (loaves >= 3)
        {
-           return (loaves / 3) * 5 + (loaves % 2) * Price;
+           return (loaves / 3) * 5 + (loaves % 3) * Price;
        }
        else
        {
            return loaves * Price;
        }
+   }
+
+   public bool outofStock (int loaves)
+   {
+       return (loaves > Loaves);
    }
 
 }
